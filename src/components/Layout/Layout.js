0521d048a99b086408ccaf17f8ad80ebd5a1fdc0
marleintje <marleinrusch@gmail.com>
2018-12-06 08:46:23 +0100

    saving course progress

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar/>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+// l. 140. const turned into class to implement method and listen to closing and opening
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSideDrawer: true // true for testing
+  }
+  // arrow function, so this keyword refers to the class at all time
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render () {
+    return ( 
+      <Aux>
+        <Toolbar/>
+        <SideDrawer 
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} 
+          />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,1 +1,36 @@
 import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
+
+// The side drawer itself and the toggle button
+
+const sideDrawer = (props) => {
+  // l. 140
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
+
+  // Conditionally attach different CSS classes to slide in and out
+  return (
+    <Aux>
+      {/* boolean value dus show = true */}
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;

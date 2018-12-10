324c2661a4bf1b844f20f1952fc24a1cd9618e3a
marleintje <marleinrusch@gmail.com>
2018-12-10 16:19:29 +0100

    Saving course progress (up to l. 230)

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,62 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
+  // l.228 (omzetting naar dit object) 
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+          value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-mail'
+        },
+        value: ''
+      },
+      deliverymethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},          
+          ]
+        },
+        value: ''
+      },
     },
     loading: false
   }
@@ -26,17 +74,7 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       // In real-world set-up this would not be safe: you'd recalculate the price on the server
-      price: this.props.price,
-      customer: {
-        name: 'Marlein',
-        address: {
-          street: 'Teststraat 1',
-          zipCode: '12345',
-          country: 'The Netherlands'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
+      price: this.props.price
     }
     // second argument 'order' is the data that gets sent
     axios.post('/orders.json', order)
@@ -53,12 +91,24 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm){
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
       <form>
-        <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-        <input className={classes.Input} type="text" name="email" placeholder="Your Email"/>
-        <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-        <input className={classes.Input} type="text" name="postal" placeholder="Postal code"/>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id} 
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
         <Button 
           btnType="Success"
           clicked={this.orderHandler}
